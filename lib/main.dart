301000e631a67ec629ee17f10f3877a4d9135035
khodary <ahmedmohamed2019.com@gmail.com>
2024-03-06 19:58:26 +0200

    change in appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class KtabyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+        appBar: AppBar(title: const Text('Ktaby App')),
       ),
     );
   }
